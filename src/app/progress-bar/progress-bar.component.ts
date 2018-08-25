ed5301d7eb4edec6b0fa017e2b50e40d7d9c0c6a
laxman20 <laxman0202@gmail.com>
2018-08-24 22:48:49 -0400

    add titles to progress bar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent implements OnInit {
+  curStep: Number;
+  stages: String[];
 
-  constructor() { }
+  constructor() {
+    this.curStep = 1;
+    this.stages = [
+      "Application",
+      "Terms",
+      "Payment Setup",
+      "Documents"
+    ];
+  }
 
   ngOnInit() {
   }
